082c778728f3506edf63f8ad08abff7159470d6f
Damir Sasivarevic <dasas@Damirs-MacBook-Pro.local>
2015-09-02 08:18:04 +0200

    first commit

---
+++ spellcheck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -n "Enter some text : "
+read mytext 
+
+echo  "$mytext" > file2.txt
+
+#tr -sc [:alpha:] '\n' < file2.txt | tr A-Z a-z | sort | uniq | comm -23 - dict.txt | wc -l
+
+comm -32 <(tr -sc [:alpha:] "\n"  < file2.txt | tr A-Z a-z | sort -u) <(tr A-Z a-z <  dict.txt |sort -u)|wc -l
